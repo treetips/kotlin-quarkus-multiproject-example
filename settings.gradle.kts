057c1934a33c80109672f39ae68f5d8c91f92189
tree <treetips555@gmail.com>
2025-01-30 00:22:03 +0900

    refactor: migrate to gradle version catalogs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,3 @@
-pluginManagement {
-  val quarkusPluginVersion: String by settings
-  val quarkusPluginId: String by settings
-  repositories {
-    mavenLocal()
-    mavenCentral()
-    gradlePluginPortal()
-  }
-  plugins { id(quarkusPluginId) version quarkusPluginVersion }
-}
-
 rootProject.name = "kotlin-quarkus-multiproject-example"
 
 include("share", "admin", "batch", "graphql-server", "grpc-server", "restfulapi-server")
